dd022223760c0e6ed08ab19aae92b9ba9663214d
AsmaaSamir5 <samaaelghareeb@gmail.com>
2022-02-18 01:22:52 +0200

    order & orderdetail

---
+++ ITech Project/ITech Project/Models/Order.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITech_Project.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Order Number")]
+        [Required(ErrorMessage = "Order Number Is Required")]
+        public int OrderNumber { get; set; }
+
+
+        [Display(Name = "Order Date")]
+        [Required(ErrorMessage = "Order Date Is Required")]
+        [DataType(DataType.Date)]
+        public string OrderDate { get; set; }
+
+        [Display(Name = "Ship Date")]
+        [Required(ErrorMessage = "Ship Date Is Required")]
+        [DataType(DataType.Date)]
+        public string ShipDate { get; set; }
+
+        [Display(Name = "Required Date")]
+        [Required(ErrorMessage = "Required Date Is Required")]
+        [DataType(DataType.Date)]
+        public string RequiredDate { get; set; }
+
+        [Display(Name = "Freight")]
+        [DataType(DataType.Currency)]
+        public int Freight { get; set; }
+
+        [Display(Name = "Timestamp")]
+        public string Timestamp { get; set; }
+
+        
+        [Display(Name = "Transact Status")]
+        [Required(ErrorMessage = "Transact Status Is Required")]
+        [DataType(DataType.CreditCard)]
+        public string TransactStatus { get; set; }
+
+        [Display(Name = "Delete Order")]
+        [Required(ErrorMessage = "Delete Order Is Required")]
+        public bool Deleted { get; set; }
+
+        [Display(Name = "Paid")]
+        [Required(ErrorMessage = "Paid Field Is Required")]
+        [DataType(DataType.Currency)]
+        public int Paid { get; set; }
+
+        [Display(Name = "Payment Date")]
+        [Required(ErrorMessage = "Payment Date Is Required")]
+        [DataType(DataType.Date)]
+        public string PaymentDate { get; set; }
+
+        /* [ForeignKey("Customer")]
+         public int Customer_Id { get; set; }
+         public Customer Customer { get; set; }
+
+         [ForeignKey("Payment")]
+         public int Payment_Id { get; set; }
+         public Payment Payment { get; set; }
+
+         [ForeignKey("Shipper")]
+         public int Shipper_Id { get; set; }
+         public Shipper Shipper { get; set; }
+         */
+
+
+
+
+    }
+}
---
+++ ITech Project/ITech Project/Models/OrderDetail.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITech_Project.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Order Number")]
+        [Required(ErrorMessage = "Order Number Is Required")]
+        public int OrderNumber { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "Price Is Required")]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+
+        [Display(Name = "Quantity")]
+        [Required(ErrorMessage = "Quantity Is Required")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Discount")]
+        public int Discount { get; set; }
+
+        [Display(Name = "Total")]
+        [Required(ErrorMessage = "Total Is Required")]
+        [DataType(DataType.Currency)]
+        public int Total { get; set; }
+
+        [Display(Name = "Model")]
+        [Required(ErrorMessage = "Model Is Required")]
+        public string Model { get; set; }
+
+        [Display(Name = "Color")]
+        [Required(ErrorMessage = "Color Is Required")]
+        public string Color { get; set; }
+
+        [Display(Name = "Ship Date")]
+        [Required(ErrorMessage = "Ship Date Is Required")]
+        [DataType(DataType.Date)]
+        public string ShipDate { get; set; }
+
+        [Display(Name = "Bill Date")]
+        [Required(ErrorMessage = "Bill Date Is Required")]
+        [DataType(DataType.Date)]
+        public string BillDate { get; set; }
+
+        /* [ForeignKey("Order")]
+         public int Order_Id { get; set; }
+         public Order Order { get; set; }
+
+         [ForeignKey("Product")]
+         public int Product_Id { get; set; }
+         public Product Product { get; set; }
+         */
+
+
+
+
+    }
+}
