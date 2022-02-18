e368e06696390c42b94a6d8bd36f5a6ae97d057c
Mayada-Ahmed <mayadaa.ahmed2810@gmail.com>
2022-02-18 22:11:21 +0200

    Payment

--- ITech Project/ITech Project/Models/Payment.cs
+++ ITech Project/ITech Project/Models/Payment.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ITech_Project.Models
 {
     public class Payment
     {
+
 
         public int ID { get; set; }
 
