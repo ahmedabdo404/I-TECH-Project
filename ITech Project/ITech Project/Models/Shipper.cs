2194e7afdf0ef29845fb80aa492a3ad5e0d9fb1a
mhgbr <mgabr0004@gmail.com>
2022-02-19 00:13:24 +0200

    Files

---
+++ ITech Project/ITech Project/Models/Shipper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITech_Project.Models
+{
+    public class Shipper
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+
+        public int Phone { get; set; }
+         
+
+        
+    }
+}
---
+++ ITech Project/ITech Project/Models/Supplier.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITech_Project.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactFName { get; set; }
+        public string ContactLName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int postalCode { get; set; }
+        public string Country { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Url { get; set; }
+        public string paymentMethod { get; set; }
+        public string TypeGoods { get; set; }
+        public bool DiscountAvaiable { get; set; }
+        public string Logo { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
