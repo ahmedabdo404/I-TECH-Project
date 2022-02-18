d65f4ef1286c3a3bd0bada2caaacc727afbc45e6
Mayada-Ahmed <mayadaa.ahmed2810@gmail.com>
2022-02-18 22:06:37 +0200

    Payment

--- ITech Project/ITech Project/Models/Payment.cs
+++ ITech Project/ITech Project/Models/Payment.cs
@@ -4,5 +4,11 @@ namespace ITech_Project.Models
 {
     public class Payment
     {
+
+        public int ID { get; set; }
+
+        [Required]
+        public string PaymentType { get; set; }
+        public bool Allowed { get; set; }
     }
 }
