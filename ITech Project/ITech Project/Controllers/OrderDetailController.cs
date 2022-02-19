e4f1561f8105b379954d8534deea0f236004f085
AsmaaSamir5 <samaaelghareeb@gmail.com>
2022-02-19 19:09:34 +0200

    Ord and OrdDetails Services & Controller

---
+++ ITech Project/ITech Project/Controllers/OrderDetailController.cs
@@ -0,0 +1,87 @@
+﻿using ITech_Project.Models;
+using ITech_Project.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ITech_Project.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        public IOrderDetailService OrderDetailServices { get; }
+        public OrderDetailController(IOrderDetailService _ordRepo)
+        {
+            OrderDetailServices = _ordRepo;
+        }
+
+
+        public IActionResult GetAll()
+        {
+            return View(OrderDetailServices.GetAll());
+        }
+
+        // Student/GetById
+        //[Route("Student/id:int")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            return View(OrderDetailServices.GetById(id));
+        }
+
+        // Student/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(OrderDetail neword)
+        {
+            if (ModelState.IsValid)
+            {
+                //save db
+                OrderDetailServices.Create(neword);
+                //display index view
+                return RedirectToAction("GetAll");
+            }
+
+            return View(neword);//html
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+
+            OrderDetail ord = OrderDetailServices.GetById(id);
+            return View(ord);
+        }
+        [HttpPost]
+        public IActionResult Update(OrderDetail newOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                OrderDetailServices.Update(newOrder);
+                return RedirectToAction("GetAll");
+            }
+
+            return View(newOrder);
+        }
+
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                OrderDetailServices.Delete(id);
+                return RedirectToAction("GetAll");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.InnerException.Message);
+                return View("Update");
+            }
+        }
+
+
+    }
+}
---
+++ ITech Project/ITech Project/Service/IOrderDetailService.cs
@@ -0,0 +1,16 @@
+﻿using ITech_Project.Models;
+using System.Collections.Generic;
+
+namespace ITech_Project.Service
+{
+    public interface IOrderDetailService
+    {
+        Db Context { get; }
+
+        int Create(OrderDetail ord);
+        int Delete(int id);
+        List<OrderDetail> GetAll();
+        OrderDetail GetById(int id);
+        int Update(OrderDetail ord);
+    }
+}
