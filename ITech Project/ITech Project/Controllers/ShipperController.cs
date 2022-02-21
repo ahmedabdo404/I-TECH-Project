2d5af9c18488710e1d6a671f506113634abb2e62
mhgbr <mgabr0004@gmail.com>
2022-02-21 15:40:52 +0200

    updated

--- ITech Project/ITech Project/Controllers/ShipperController.cs
+++ ITech Project/ITech Project/Controllers/ShipperController.cs
@@ -13,9 +13,83 @@ namespace ITech_Project.Controllers
             this.shipperService = shipperService;
         }
 
-        public IActionResult Index()
+        public IActionResult GetAll()
+        {
+            return View(shipperService.GetAll());   
+        }
+
+        public IActionResult GetById(int id)
+        { 
+        return View(shipperService.GetById(id));
+        }
+
+        public IActionResult GetByName(string name)
+        { 
+        return View(shipperService.GetByName(name));
+        }
+            
+            
+            [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Shipper ship)
+        {
+            if (ModelState.IsValid)
+            { 
+            shipperService.Create(ship);
+            return RedirectToAction("GetAll");
+            }
+            return View(ship);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(shipperService.GetById(id));
+        }
+        [HttpPost]
+
+        public IActionResult Update(Shipper ship)
+        {
+            if (ModelState.IsValid)
+            { 
+            shipperService.Update(ship);
+                return RedirectToAction("GetAll");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                shipperService.Delete(id);
+                return RedirectToAction("GetAll");
+            }
+            catch //(System.Exception)
+            {
+                ModelState.AddModelError("", "this category is in use");
+                return View("Update");
+            }
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ ITech Project/ITech Project/Controllers/SupplierController.cs
@@ -0,0 +1,84 @@
+﻿using ITech_Project.Models;
+using ITech_Project.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITech_Project.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly ISupplierService supplier;
+
+        public SupplierController(ISupplierService supplier)
+        {
+            this.supplier = supplier;
+        }
+
+        public IActionResult GetAll()
+        { 
+        
+        return View(supplier.GetAll());
+        }
+
+        public IActionResult GetById(int id)
+        { 
+        return View(supplier.GetById(id));  
+        }
+
+        public IActionResult GetByName(string name)
+        { 
+        return View(supplier.GetByName(name));
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Supplier sup)
+        {
+            if (ModelState.IsValid)
+            { 
+            supplier.Create(sup);
+                return RedirectToAction("GetAll");
+            }
+            return View(sup);
+        }
+
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(supplier.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Supplier sup)
+        {
+            if (ModelState.IsValid)
+            { 
+            supplier.Update(sup);
+                return RedirectToAction("GetAll");
+            }
+            return View();        
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                supplier.Delete(id);
+                return RedirectToAction("GetAll");
+
+            }
+            catch //(System.Exception)
+            {
+                ModelState.AddModelError("", "this category is in use");
+                return View("Update");
+            }
+        
+        }
+
+    }
+}
