52e3eabf67fc597cccda281aefe821d2f736c1fa
Mayada-Ahmed <mayadaa.ahmed2810@gmail.com>
2022-03-04 18:36:42 +0200

    mayada

--- ITech Project/ITech Project/Controllers/AccountController.cs
+++ ITech Project/ITech Project/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
 using System.Net.Mail;
 using System.Security.Claims;
-
+using System.Linq;
 
 namespace ITech_Project.Controllers
 {
@@ -67,7 +67,6 @@ namespace ITech_Project.Controllers
 
 
         #endregion 
-
 
         #region Sign Up Supplier
 
@@ -186,28 +185,33 @@ namespace ITech_Project.Controllers
         #endregion
 
         #region Login
-
-        [HttpGet]
         //public IActionResult Login(string ReturnUrl = "~/Home/index")
         //{
-        //    ViewData["ReturnUrl"] = ReturnUrl;
-        //    return View();
+        //    if (!User.Identity.IsAuthenticated)
+        //    {
+        //        ViewData["ReturnUrl"] = ReturnUrl;
+        //        return View();
+        //    }
+        //    return RedirectToAction("index", "Home");
         //}
-        public IActionResult Login(string ReturnUrl = "~/Home/index")
-        {
-            if (!User.Identity.IsAuthenticated)
-            {
-                ViewData["ReturnUrl"] = ReturnUrl;
-                return View();
-            }
-            return RedirectToAction("index", "Home");
-        }
 
 
         //Check create cookie
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            LoginViewModel model = new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+            return View(model);
+        }
+
+
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel LoginUser,
-            string ReturnUrl = "~/Home/index")
+        public async Task<IActionResult> Login(LoginViewModel LoginUser, string ReturnUrl = "~/Home/index")
         {
             if (ModelState.IsValid)
             {
@@ -249,6 +253,94 @@ namespace ITech_Project.Controllers
         }
 
 
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
+            var properties = signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
+
+
+        [AllowAnonymous]
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            LoginViewModel loginViewModel = new LoginViewModel
+            {
+                ReturnUrl = returnUrl,ExternalLogins = (await signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+
+            if (remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error from external provider: {remoteError}");
+
+                return View("Login", loginViewModel);
+            }
+
+            // Get the login information about the user from the external login provider
+            var info = await signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                ModelState.AddModelError(string.Empty, "Error loading external login information.");
+
+                return View("Login", loginViewModel);
+            }
+
+            // If the user already has a login (i.e if there is a record in AspNetUserLogins
+            // table) then sign-in the user with this external login provider
+            var signInResult = await signInManager.ExternalLoginSignInAsync(info.LoginProvider,
+                info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+
+            if (signInResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            // If there is no record in AspNetUserLogins table, the user may not have
+            // a local account
+            else
+            {
+                // Get the email claim value
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+
+                if (email != null)
+                {
+                    // Create a new user without password if we do not have a user already
+                    var user = await userManager.FindByEmailAsync(email);
+
+                    if (user == null)
+                    {
+                        user = new IdentityUser
+                        {
+                            UserName = info.Principal.FindFirstValue(ClaimTypes.Email),
+                            Email = info.Principal.FindFirstValue(ClaimTypes.Email)
+                        };
+
+                        await userManager.CreateAsync(user);
+                    }
+
+                    // Add a login (i.e insert a row for the user in AspNetUserLogins table)
+                    await userManager.AddLoginAsync(user, info);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+
+                    return LocalRedirect(returnUrl);
+                }
+
+                // If we cannot find the user email we cannot continue
+                ViewBag.ErrorTitle = $"Email claim not received from: {info.LoginProvider}";
+                ViewBag.ErrorMessage = "Please contact support on Pragim@PragimTech.com";
+
+                return View("Error");
+            }
+        }
+
+
+
+
+
         #endregion
 
         #region Logout
@@ -261,104 +353,5 @@ namespace ITech_Project.Controllers
         }
 
         #endregion
-
-        #region Forget Password
-
-        [HttpGet]
-        public IActionResult ForgetPassword()
-        {
-            return View();
-        }
-
-        //&& await userManager.IsEmailConfirmedAsync(user)
-
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel userMail)
-        {
-            if (ModelState.IsValid)
-            {
-                //Generate Password Reset Token
-                var user = await userManager.FindByEmailAsync(userMail.Email);
-
-                if (user != null)
-                {
-                    var token = await userManager.GeneratePasswordResetTokenAsync(user);
-
-                    //Build Password Reset Link [URL] TO Action and Controller
-
-                    //var PasswordResetLink = Url.Action("ResetPassword", "Account", new { email = userMail.Email, token = token },
-                    //    Request.Scheme);
-
-                    var PasswordResetLink = $"https://localhost:{44302}/Account/ResetPassword?email={userMail.Email}&token={token}";
-
-                    //Request.scheme => it generates the request scheme and it's required to generate the full absolute url
-
-                    //Log Password Reset Link
-
-                    return View("ForgetPasswordConfirmation");
-                }
-                return View("ForgetPasswordConfirmation");
-            }
-            return View(userMail);
-        }
-        //public async Task<IActionResult> ForgetPassword(string UserMail)
-        //{
-
-        //        if (!ModelState.IsValid)
-        //            return View(UserMail);
-
-        //        var user = await userManager.FindByEmailAsync(UserMail);
-        //        if (user == null)
-        //            return RedirectToAction("ForgotPasswordConfirmation");
-
-        //        var token = await userManager.GeneratePasswordResetTokenAsync(user);
-        //        var link = Url.Action("ResetPassword", "Account", new { token, email = user.Email }, Request.Scheme);
-
-        //        EmailHelper emailHelper = new EmailHelper();
-        //        bool emailResponse = emailHelper.SendEmailPasswordReset(user.Email, link);
-
-        //        if (emailResponse)
-        //            return RedirectToAction("ForgotPasswordConfirmation");
-        //        else
-        //        {
-        //            // log email failed 
-        //        }
-        //        return View(UserMail);
-
-        //}
-        #endregion
-
-        //#region Change Password
-
-
-        //public IActionResult ChangePassword()
-        //{
-        //    return View();
-        //}
-
-        //[HttpPost("change-password")]
-        //public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var result = await sig.ChangePasswordAsync(model);
-        //        if (result.Succeeded)
-        //        {
-        //            ViewBag.IsSuccess = true;
-        //            ModelState.Clear();
-        //            signInManager.c
-        //            return View();
-        //        }
-
-        //        foreach (var error in result.Errors)
-        //        {
-        //            ModelState.AddModelError("", error.Description);
-        //        }
-
-        //    }
-        //    return View(model);
-        //}
-        //#endregion
     }
 }
